abb60c085f9da3d07875018497e0abee062ceb00
storku <36249562+storku@users.noreply.github.com>
2018-03-31 08:18:27 -0400

    Add detecting location indicator

--- client/src/components/frontpage/AutoDetect.js
+++ client/src/components/frontpage/AutoDetect.js
@@ -8,11 +8,17 @@ class AutoDetect extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      detecting: false
+    };
+
     this.handleClick = this.handleClick.bind(this);
     this.success = this.success.bind(this);
   }
 
   success(position) {
+    console.log('location has been detected');
+    this.setState({ detecting: false });
     const coords = position.coords;
     this.props.fetchLocation(coords);
     this.props.history.push('/reps');
@@ -21,10 +27,17 @@ class AutoDetect extends Component {
   handleClick(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(this.success);
+    this.setState({ detecting: true });
+    console.log('detecting location');
   }
 
   render() {
-    return <Button onClick={this.handleClick}>Detect Your Reps!</Button>;
+    return (
+      <div>
+        {this.state.detecting ? <h5>Currently Detecting Your Location</h5> : ''}
+        <Button onClick={this.handleClick}>Detect Your Reps!</Button>
+      </div>
+    );
   }
 }
 
